6655fa6192c1a7bc9a4ac956f5ef13d5029facf4
Sandesh Athawale <61369792+sandesh1409@users.noreply.github.com>
2022-04-03 19:14:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
+import pandas as pd
+import numpy as np
 
-app = Flask(__name__)#,template_folder='template')
+app = Flask(__name__,template_folder='template')
 #model = pickle.load(open('modelfile1.pkl', 'rb'))
-mod = 'modelfile1.pkl'
+mod = 'modelfile.pkl'
 with open(mod, 'rb') as d:
     model = pickle.load(d)
     
@@ -12,37 +14,33 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/predict", methods=["POST"])#,"GET"])
-def process():
-    #if request.method == "POST":
+@app.route("/predict", methods=["POST"])
 
-        signup_date = request.form.get("signup_date")
-        ref_date = request.form.get("ref_date")
-        money = request.form.get("money")
-        time = request.form.get("time")
-        country = request.form.get("country")
+def process():
+    
+    int_features = [x for x in request.form.values()]
         
-        no_of_day = ref_date - signup_date
-        days = no_of_day.days
+    signup_date = pd.to_datetime(int_features[0])
+    ref_date = pd.to_datetime(int_features[1])
+    no_of_day = ref_date - signup_date
+    day = no_of_day.days
         
-        int_features = [[money, time, days]]
-        prediction = model.predict(int_features)
+    time = pd.to_numeric(int_features[2])
+    money = pd.to_numeric(int_features[3])
         
         
-        if prediction[0] < 0.5:
-            return render_template('index.html', prediction_text = 'customer will churn')
-        else:
-            return render_template('index.html', prediction_text = 'customer does not churn')
+    int_fea = np.array([[money, time, day]])
+    prediction = model.predict(int_fea)
         
+        
+    if prediction < 0.5:
+        return render_template('index.html', prediction_text = 'customer will churn')
+    else:
+        return render_template('index.html', prediction_text = 'customer does not churn')
+    
+
 
 if __name__ == "__main__":
-    #from waitress import serve
-    #serve(app, host="0.0.0.0", port=8080)
+    
     app.run(debug=True)
-    
-        
-    
-
-
-    
     
